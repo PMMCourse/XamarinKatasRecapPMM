709202f8c40f6bd6ccb7368930241e0ac92b87d4
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-09 17:38:20 +0200

    Kata UI A

--- XamarinFormsKatas/XamarinFormsKatas/App.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/App.xaml.cs
@@ -10,8 +10,7 @@ namespace XamarinFormsKatas
         public App()
         {
             InitializeComponent();
-
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new Katas_UI.Kata_a.KataA());
         }
 
         protected override void OnStart()
