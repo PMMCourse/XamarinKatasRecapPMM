62be7ae927b3960ce8fdae0af7c70dce54ed07ef
Natalia <Natalia@NATALIA-HP>
2020-05-10 20:15:42 +0200

    Ejer R

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_r/KataR.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_r/KataR.xaml.cs
@@ -12,9 +12,24 @@ namespace XamarinFormsKatas.Katas_UI.Kata_r
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataR : ContentPage
 	{
+        List<string> listaElementos = new List<string>();
 		public KataR ()
 		{
 			InitializeComponent ();
-		}
+            Cliente cliente1 = new Cliente("Marina","15225124R");
+            Cliente cliente2 = new Cliente("Juan", "51824562G");
+            Cliente cliente3 = new Cliente("Luisa", "19751999F");
+            Proveedores proveedor1 = new Proveedores("Ramon", "Jamon");
+            Proveedores proveedor2 = new Proveedores("Eduardo", "Licores");
+            Proveedores proveedor3 = new Proveedores("Paula", "Flores");
+            listaElementos.Add(cliente1.ToString());
+            listaElementos.Add(cliente2.ToString());
+            listaElementos.Add(cliente3.ToString());
+            listaElementos.Add(proveedor1.ToString());
+            listaElementos.Add(proveedor2.ToString());
+            listaElementos.Add(proveedor3.ToString());
+
+            listaPrincipal.ItemsSource = listaElementos;
+        }
 	}
 }
