e15f2b727225b18327cb94995d708001921a9dd4
Natalia <Natalia@NATALIA-HP>
2020-05-10 19:56:17 +0200

    Ejer Q

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_q/EntryRenderer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_q
+{
+    public class EntryRenderer:EntryRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<Entry>e)
+        {
+            base.OnElementChanged(e);
+            if (Control != null)
+            {
+                Control.BorderThickness = new Thickness(0);
+            }
+        }
+    }
+}
