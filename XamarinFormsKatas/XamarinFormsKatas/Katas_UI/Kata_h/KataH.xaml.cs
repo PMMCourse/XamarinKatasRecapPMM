9714b148b5fdd934c6a4b508fa106f4e15fcb31c
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-11 15:23:32 +0200

    Kata UI H

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_h/KataH.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_h/KataH.xaml.cs
@@ -15,6 +15,15 @@ namespace XamarinFormsKatas.Katas_UI.Kata_h
 		public KataH ()
 		{
 			InitializeComponent ();
+			BtnAnimated.Clicked += async (sender, e) =>
+			{
+				await BtnAnimated.TranslateTo(0, -100, 100);
+				await BtnAnimated.RotateTo(720, 1000);
+				await BtnAnimated.TranslateTo(0, 0, 100);
+
+				BtnAnimated.Rotation = 0;
+				BtnAnimated.TranslationX = 0;
+			};
 		}
 	}
 }
