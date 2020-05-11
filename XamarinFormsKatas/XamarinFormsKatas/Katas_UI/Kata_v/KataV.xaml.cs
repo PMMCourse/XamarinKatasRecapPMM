1fee115d1537fa0d9d85007703f46d5d22872654
Natalia <Natalia@NATALIA-HP>
2020-05-11 11:57:39 +0200

    Ejer U Mapa Mejorado

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_v/KataV.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_v/KataV.xaml.cs
@@ -14,28 +14,7 @@ namespace XamarinFormsKatas.Katas_UI.Kata_v
 	{
 		public KataV ()
 		{
-			InitializeComponent ();
-            var mapa = new Map(MapSpan.FromCenterAndRadius(new Position(37.393355, -5.983900), Distance.FromMiles(1)))
-            {
-                VerticalOptions=LayoutOptions.FillAndExpand
-            };
-            var pFesac = new Position(37.371861, -5.999654);
-            var pcampoFutbol = new Position(37.384217, -5.993467);
-            var pin1 = new Pin
-            {
-                Type = PinType.Place,
-                Position =pFesac ,
-                Label="Fesac"
-            };
-            var pin2 = new Pin
-            {
-                Type =PinType.Place,
-               Position = pcampoFutbol,
-               Label ="Campo Futbol"
-            };
-            mapa.Pins.Add(pin1);
-            mapa.Pins.Add(pin2);
-            mapaPrincipal.Children.Add(mapa);
+            InitializeComponent();
         }
 	}
 }
