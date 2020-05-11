29c89d6422e79a03a4ead9703836949c962d6088
Natalia <Natalia@NATALIA-HP>
2020-05-11 17:10:27 +0200

    Ejer LC

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LC/MainViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LC
+{
+    public class MainViewModel: baseViewModel
+    {
+        private Command _command;
+        public ICommand command => _command;
+
+        public MainViewModel()
+        {
+            _command = new Command(checkButton);
+        }
+        async void checkButton()
+        {
+            await Application.Current.MainPage.DisplayAlert("titulo", "message", "Cancelar");
+
+        }
+    }
+}
