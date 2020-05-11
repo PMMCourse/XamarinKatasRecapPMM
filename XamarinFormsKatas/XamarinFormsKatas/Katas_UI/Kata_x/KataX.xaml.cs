435f557d230caf05b1cd9b0a7df05687d3b0fea3
Natalia <Natalia@NATALIA-HP>
2020-05-11 12:27:17 +0200

    Ejer X

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_x/KataX.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_x/KataX.xaml.cs
@@ -12,9 +12,24 @@ namespace XamarinFormsKatas.Katas_UI.Kata_x
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataX : ContentPage
 	{
+        List<int> lista = new List<int>();
 		public KataX ()
 		{
 			InitializeComponent ();
 		}
-	}
+        public void CrearObjeto(objeto sender, FocusEventArgs e)
+        {
+            var objeto = new Picker();
+            objeto.ItemsSource = lista;
+            ejercicio.Children.Add(objeto);
+        }
+        public void rellenarLista()
+        {
+            lista.Add(28);
+            lista.Add(16);
+            lista.Add(5);
+            lista.Add(12);
+            lista.Add(33);
+        }
+    }
 }
