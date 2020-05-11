aebfe03968785932a7c66357f6f7b7b070da0395
Natalia <Natalia@NATALIA-HP>
2020-05-11 11:25:28 +0200

    Ejer T

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/Deportista.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_UI.Kata_t
+{
+    public class Deportista
+    {
+        public int puntuacion { get; set; }
+        public string nombre { get; set; }
+        public string  deporte { get; set; }
+        public int edad { get; set; }
+        public Deportista(int Puntuacion, string Nombre, string Deporte, int Edad)
+        {
+            puntuacion = Puntuacion;
+            nombre = Nombre;
+            deporte = Deporte;
+            edad = Edad;
+        }
+        public Deportista()
+        {
+
+        }
+        public void ToString()
+        {
+            return $"puntuacion: {puntuacion}, nombre: {nombre}, deporte: {deporte}, edad: {edad}";
+        }
+    }
+}
--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/KataT.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_t/KataT.xaml.cs
@@ -12,9 +12,18 @@ namespace XamarinFormsKatas.Katas_UI.Kata_t
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataT : ContentPage
 	{
+        List<Deportista> lista = new List<Deportista>();
 		public KataT ()
 		{
 			InitializeComponent ();
+            Deportista dpt1 = new Deportista(28, "Juan", "tenis", 28);
+            Deportista dpt2 = new Deportista(52, "Martin", "baloncesto", 20);
+            Deportista dpt3 = new Deportista(30, "Pau", "futbol", 34);
+            lista.Add(dpt1.ToString());
+            lista.Add(dpt2.ToString());
+            lista.Add(dpt3.ToString());
+            listaPrincipal.ItemsSource = lista;
 		}
+
 	}
 }
