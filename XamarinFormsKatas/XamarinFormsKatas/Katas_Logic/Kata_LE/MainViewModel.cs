0fde742e383e034d7e5cfc10e44484f0dd2e0cc9
Natalia <Natalia@NATALIA-HP>
2020-05-11 17:40:27 +0200

    Ejer LE

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LE/MainViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LE
+{
+    public class MainViewModel:BindableObject
+    {
+        private byte? _rojo;
+        public byte? rojo
+        {
+            get => _rojo;
+            set
+            {
+                _rojo = value;
+                OnPropertyChanged();
+            }
+        }
+        private byte? _verde;
+        public byte? verde
+        {
+            get => _verde;
+            set
+            {
+                _verde = value;
+                OnPropertyChanged();
+            }
+        }
+        private byte? _azul;
+        public byte? azul
+        {
+            get => _azul;
+            set
+            {
+                _azul = value;
+                OnPropertyChanged();
+            }
+        }
+        private Color _colorMexcla;
+        public Color colorMexcla
+        {
+            get => _colorMexcla;
+            set
+            {
+                _colorMexcla = value;
+                OnPropertyChanged();
+            }
+        }
+        public MainViewModel()
+        {
+            _mexclaColoresCommand = new Command(PerformBlendColors);
+        }
+        private Command _mexclaColoresCommand;
+        public ICommand mexclaColoresCommand => _mexclaColoresCommand;
+        private void PerformBlendColors(object obj)
+        {
+            if(_rojo.HasValue&& _verde.HasValue && _azul.HasValue)
+            {
+                colorMexcla = Color.FromRgb(_rojo.value, _verde.Value, _azul.Value);
+            }
+        }
+    }
+}
