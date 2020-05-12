61451277ffa1b26697002c5e8eb569eff3a3f78c
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-12 16:59:37 +0200

    Kata UI O

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_o/KataO.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_o/KataO.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,9 +13,24 @@ namespace XamarinFormsKatas.Katas_UI.Kata_o
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class KataO : ContentPage
 	{
-		public KataO ()
-		{
-			InitializeComponent ();
-		}
-	}
+        public KataO()
+        {
+            InitializeComponent();
+            this.Disappearing += OnBackButtonPressed;
+        }
+        private void OnBackButtonPressed(object sender, EventArgs e)
+        {
+            Device.BeginInvokeOnMainThread(async () =>
+            {
+                var resultado = await this.DisplayAlert("ALERTA!", "¿Seguro que quieres salir?", "Salir", "Permanecer en esta pagina");
+                if (resultado == true)
+                {
+                    //await this.Navigation.PopAsync();
+                    Process.GetCurrentProcess().CloseMainWindow();
+                }
+
+            });
+
+        }
+    }
 }
