ddf969c180ab1d74e0edab2043cea74640f887d1
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-14 20:45:22 +0200

    Kata UI W

--- XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_UI/Kata_w/KataW.xaml.cs
@@ -16,5 +16,28 @@ namespace XamarinFormsKatas.Katas_UI.Kata_w
 		{
 			InitializeComponent ();
 		}
-	}
+        private void clickBoton(object sender, EventArgs e)
+        {
+            EControl.IsVisible = false;
+            LControl.IsVisible = false;
+            BControl.IsVisible = false;
+            AControl.IsVisible = false;
+            if (switchE.IsToggled)
+            {
+                EControl.IsVisible = true;
+            }
+            if (switchL.IsToggled)
+            {
+                LControl.IsVisible = true;
+            }
+            if (switchB.IsToggled)
+            {
+                BControl.IsVisible = true;
+            }
+            if (switchA.IsToggled)
+            {
+                AControl.IsVisible = true;
+            }
+        }
+    }
 }
