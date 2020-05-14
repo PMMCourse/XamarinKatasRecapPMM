c8479687f065dfe15a1bed04c0eddc425a788d03
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-14 21:37:51 +0200

    Kata Logica C

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LC/MainViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+using XamarinFormsKatas.Katas_Logic.Kata_LA;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LC
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private Command _btnKataL;
+        public ICommand btnKataL => _btnKataL;
+
+        public MainViewModel()
+        {
+            _btnKataL = new Command(TextButton);
+        }
+
+        async void TextButton(object obj)
+        {
+            await Application.Current.MainPage.DisplayAlert("titulo del mensaje", "Cuerpo del mensaje", "Cancelar");
+        }
+    }
+}
