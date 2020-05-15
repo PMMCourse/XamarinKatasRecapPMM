8b22c0364696ab7b2b10a2a79d144e2458132bab
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-15 20:09:06 +0200

    Kata Logica D

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LD/Page1ViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using XamarinFormsKatas.Katas_Logic.Kata_LA;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LD
+{
+    public class Page1ViewModel : BaseViewModel
+    {
+        private string _txt;
+
+        public string txt
+        {
+            get => _txt;
+            set
+            {
+                _txt = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public Page1ViewModel(string textoo)
+        {
+            this.txt = textoo;
+        }
+    }
+}
