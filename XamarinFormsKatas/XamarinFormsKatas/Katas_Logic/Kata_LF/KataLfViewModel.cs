d364f301b8a1513be9a9d1a28073e40cd9e3f8e5
Juan Carlos Cárdenas García <Jccarcia98@gmail.com>
2020-05-15 20:35:07 +0200

    Kata Logica F

---
+++ XamarinFormsKatas/XamarinFormsKatas/Katas_Logic/Kata_LF/KataLfViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using XamarinFormsKatas.Katas_Logic.Kata_LA;
+
+namespace XamarinFormsKatas.Katas_Logic.Kata_LF
+{
+    public class KataLfViewModel : BaseViewModel
+    {
+        private ObservableCollection<Deportistas> _deportistas = new ObservableCollection<Deportistas>();
+        public ObservableCollection<Deportistas> deportistas
+        {
+            get => _deportistas;
+            set
+            {
+                _deportistas = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public KataLfViewModel()
+        {
+            _deportistas.Add(new Deportistas() { Deporte = "Futbol", Edad = 22, Puntuacion = 80, Sexo = "Hombre" });
+            _deportistas.Add(new Deportistas() { Deporte = "Futbol", Edad = 23, Puntuacion = 100, Sexo = "Mujer" });
+            _deportistas.Add(new Deportistas() { Deporte = "Futbol", Edad = 22, Puntuacion = 40, Sexo = "Hombre" });
+            _deportistas.Add(new Deportistas() { Deporte = "Baloncesto", Edad = 29, Puntuacion = 60, Sexo = "Mujer" });
+
+            _deportistas = new ObservableCollection<Deportistas>(_deportistas.Where(x => x.Deporte.Contains("F") && x.Puntuacion > 50));
+        }
+    }
+}
